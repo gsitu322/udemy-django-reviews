81afb8d0b18674743bb728204408c5136a8e1b23
Gordon Situ <gsitu322@gmail.com>
2025-08-25 16:55:04 -0700

    Added thank you page and redirect after post request

--- reviews/views.py
+++ reviews/views.py
@@ -1,6 +1,17 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
 
 # Create your views here.
 
 def review(request):
+    if request.method == 'POST':
+        entered_username = request.POST['username']
+        print(entered_username)
+        return HttpResponseRedirect(reverse("thank_you"))
+
     return render(request, "reviews/review.html")
+
+
+def thank_you(request):
+    return render(request, "reviews/thank_you.html")
