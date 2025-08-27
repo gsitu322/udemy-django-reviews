2e45e7fe66497df3b8abd76ed5731735d6a44c66
Gordon Situ <gsitu322@gmail.com>
2025-08-27 09:42:14 -0700

    Added more form fields and rendering all manually

--- reviews/forms.py
+++ reviews/forms.py
@@ -6,3 +6,7 @@ class ReviewForm(forms.Form):
         "required": "Please enter your name",
         "max_length": "Name must be less than 100 characters long"
     })
+
+    review_text = forms.CharField(label="Your Feedback", widget=forms.Textarea, max_length=200)
+
+    rating = forms.IntegerField(label="Your Rating", min_value=1, max_value=5)
