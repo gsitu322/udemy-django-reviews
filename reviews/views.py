2e4f4f958738766731b8bf9ef35248f9e715b978
Gordon Situ <gsitu322@gmail.com>
2025-08-27 10:40:41 -0700

    Using simple generic class based view

--- reviews/views.py
+++ reviews/views.py
@@ -2,24 +2,44 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
 from .forms import ReviewForm
+from django.views import View
 
 # Create your views here.
+class ReviewView(View):
+    def get(self, request):
+        form = ReviewForm()
 
-def review(request):
-    if request.method == 'POST':
+        return render(request, "reviews/review.html", {
+            'form': form
+        })
+
+    def post(self, request):
         form = ReviewForm(request.POST)
 
         if form.is_valid():
             form.save()
             print(form.cleaned_data)
             return HttpResponseRedirect(reverse("thank_you"))
-    else:
-        form = ReviewForm()
 
-    return render(request, "reviews/review.html", {
-        'form' : form
-    })
+        return render(request, "reviews/review.html", {
+            'form': form
+        })
 
+# Changed to use a class based view
+# def review(request):
+#     if request.method == 'POST':
+#         form = ReviewForm(request.POST)
+#
+#         if form.is_valid():
+#             form.save()
+#             print(form.cleaned_data)
+#             return HttpResponseRedirect(reverse("thank_you"))
+#     else:
+#         form = ReviewForm()
+#
+#     return render(request, "reviews/review.html", {
+#         'form' : form
+#     })
 
 def thank_you(request):
     return render(request, "reviews/thank_you.html")
