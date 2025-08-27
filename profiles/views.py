3a9c8cc96f7cc7b29d2abff6e86e24736d0a3493
Gordon Situ <gsitu322@gmail.com>
2025-08-27 14:32:06 -0700

    Using model form to save file

--- profiles/views.py
+++ profiles/views.py
@@ -1,18 +1,32 @@
 from django.shortcuts import render
 from django.views import View
 from django.http import HttpResponseRedirect
+from .forms import ProfileForm
+from .models import UserProfile
 
 # Create your views here.
 
-def store_file(file):
-    with open("temp/image.jpg", "wb+") as dest:
-        for chunk in file.chunks():
-            dest.write(chunk)
+# Don't need this anymore as ProfileForm will save it for
+# def store_file(file):
+#     with open("temp/image.jpg", "wb+") as dest:
+#         for chunk in file.chunks():
+#             dest.write(chunk)
 
 class CreateProfileView(View):
     def get(self, request):
-        return render(request, "profiles/create_profile.html")
+        return render(request, "profiles/create_profile.html", {
+            "form" : ProfileForm()
+        })
 
     def post(self, request):
-        store_file(request.FILES['image'])
-        return HttpResponseRedirect("/profiles")
+        submitted_form = ProfileForm(request.POST, request.FILES)
+
+        if submitted_form.is_valid():
+            profile = UserProfile(user_image=request.FILES['user_image'])
+            profile.save()
+
+            return HttpResponseRedirect("/profiles")
+
+        return render(request, "profiles/create_profile.html", {
+            "form" : submitted_form
+        })
