cce8410a55ded058093009bae39110ff6af584f9
Gordon Situ <gsitu322@gmail.com>
2025-08-27 10:28:25 -0700

    Using a model form

--- reviews/forms.py
+++ reviews/forms.py
@@ -1,12 +1,30 @@
 from django import forms
+from reviews.models import Review
 
 
-class ReviewForm(forms.Form):
-    user_name = forms.CharField(label="Your Name", max_length=100, error_messages={
-        "required": "Please enter your name",
-        "max_length": "Name must be less than 100 characters long"
-    })
+# class ReviewForm(forms.Form):
+#     user_name = forms.CharField(label="Your Name", max_length=100, error_messages={
+#         "required": "Please enter your name",
+#         "max_length": "Name must be less than 100 characters long"
+#     })
+# 
+#     review_text = forms.CharField(label="Your Feedback", widget=forms.Textarea, max_length=200)
+# 
+#     rating = forms.IntegerField(label="Your Rating", min_value=1, max_value=5)
 
-    review_text = forms.CharField(label="Your Feedback", widget=forms.Textarea, max_length=200)
 
-    rating = forms.IntegerField(label="Your Rating", min_value=1, max_value=5)
+class ReviewForm(forms.ModelForm):
+    class Meta:
+        model = Review
+        fields = "__all__"
+        labels = {
+            "user_name": "Your Name",
+            "review_text": "Your Feedback",
+            "rating": "Your Rating"
+        }
+        error_messages = {
+            "user_name": {
+                "required": "This field is required",
+                "max_length": "Name must be less than 100 characters long",
+            }
+        }
