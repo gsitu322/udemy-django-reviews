4583717fb0e5178dd07b8f0c85e60fe60f8af87d
Gordon Situ <gsitu322@gmail.com>
2025-08-27 10:32:18 -0700

    Use form model to save form

--- reviews/views.py
+++ reviews/views.py
@@ -2,7 +2,6 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
 from .forms import ReviewForm
-from .models import Review
 
 # Create your views here.
 
@@ -11,13 +10,7 @@ def review(request):
         form = ReviewForm(request.POST)
 
         if form.is_valid():
-            review = Review(
-                user_name=form.cleaned_data['user_name'],
-                review_text=form.cleaned_data['review_text'],
-                rating=form.cleaned_data['rating']
-            )
-            review.save()
-
+            form.save()
             print(form.cleaned_data)
             return HttpResponseRedirect(reverse("thank_you"))
     else:
